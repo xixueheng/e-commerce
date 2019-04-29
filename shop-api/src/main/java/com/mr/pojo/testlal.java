a419c66a3832dd34a7f6c23bf99da110aa065046
xueheng <1770948628@qq.com>
2019-04-29 10:04:56 +0800

    席雪恒测试

---
+++ shop-api/src/main/java/com/mr/pojo/testlal.java
@@ -0,0 +1,10 @@
+package com.mr.pojo;
+
+/**
+ * Created by asus on 2019/4/29.
+ */
+public class testlal {
+    public static void main(String[] args) {
+        System.out.println("测试席雪恒");
+    }
+}
